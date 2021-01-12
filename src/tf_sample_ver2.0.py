33be4aeb220e97da13e8c73f5ff2f6390be9c162
haxhimitsu <hachimitsu.lab@gmail.com>
2021-01-12 16:36:11 +0900

    add_dl_classification_prog and util modure

---
+++ src/tf_sample_ver2.0.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python3
+# coding: UTF-8
+#---------------------------------------------------------------
+# author:"Haxhimitsu"
+# date  :"2021/01/06"
+# cite  :
+# sample:python3 imgtrim_gui_ver.2.0.py  --input_dir ../assets/original_img/cbn_test_01/ --output_dir ../assets/sample_output/  --trim_width 32 --trim_height 64
+#---------------------------------------------------------------
+
+import keras
+from keras.utils import np_utils
+from keras.layers.convolutional import Conv2D, MaxPooling2D
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.preprocessing.image import array_to_img, img_to_array, load_img
+import keras.callbacks
+from keras.models import Sequential, model_from_json
+import tensorflow as tf
+from keras.backend.tensorflow_backend import set_session
+from tensorflow.keras.callbacks import EarlyStopping
+
+import numpy as np
+#import pandas as pd
+#from sklearn.model_selection import train_test_split
+#import matplotlib.pyplot as plt
+import cv2
+import os
+import csv
+import copy
+import random
+import argparse
+
+from utils.myutils import myutil
+
+myutil=myutil()
+myutil.sayStr("Hello")
+
+
+#################setting GPU useage#####################
+config = tf.ConfigProto(
+    gpu_options=tf.GPUOptions(per_process_gpu_memory_fraction=0.9, # 最大値の80%まで
+        allow_growth=True # True->必要になったら確保, False->全部
+      ))
+sess = sess = tf.Session(config=config)
+
+#####################################################
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--train_path", help="path to folder containing images")
+parser.add_argument("--val_path",  help="set image size e.g.'0.5,0.8...'")
+parser.add_argument("--max_epochs", type =int ,default=100,help="set trim width")
+parser.add_argument("--save_weight_name", help="set trim height")
+parser.add_argument("--save_weight_path",  help="output path",)
+a = parser.parse_args()
+
+model_weight_path=a.save_weight_path
+myutil.create_directory(model_weight_path)
+weight_filename=a.save_weight_name+".hdf5"
+max_epochs=a.max_epochs
+
+print(len(os.listdir(a.train_path)))
+
+model=myutil.create_network(category_num=len(os.listdir(a.train_path)))
+try:
+    model.load_weights(os.path.join(model_weight_path,weight_filename))#学習結果がある場合，weightを読み込み
+except OSError:
+    print(".h5 file not found")
+    print("start loading the data set")
+
+    train_img,train_label,val_img,val_label=myutil.create_dataset(a.train_path,a.val_path)
+
+    ###################EalyStopping#######################
+    """
+    検証データｎ対する誤差が増加してくるタイミングが訓練データにオーバーフィッティング
+    し始めているタイミングと考えることができるので，エポックごとの検証データに対する誤差の値を監視し，
+    一定のエポック数連続して誤差がそれまでの最小値をしたまわることがなければ打ち切る．
+    monitor='監視する値の指定'
+    patience='監視している値が何エポック連続で上回ったら早期終了するか'
+    verbose='早期終了したかどうかをログで出力するか'
+    """
+    es = EarlyStopping(monitor='val_loss',
+                        patience=5,
+                        verbose=1)
+
+    # コンパイル
+    model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
+
+    #####################################################
+
+
+    history = model.fit(train_img, train_label, batch_size=20, epochs=max_epochs,
+                    validation_data = (val_img, val_label), verbose = 1,callbacks=[es])#学習開始　パラメータは名前から察して
+    
+    model.save_weights(os.path.join(model_weight_path,weight_filename))#このコードがあるフォルダに重みを保存する
+    
+    score = model.evaluate(val_img, val_label, verbose=0)
+    print('Test loss :', score[0])
+    print('Test accuracy :', score[1])
+
--- src/utils/myutils.py
+++ src/utils/myutils.py
@@ -7,8 +7,119 @@
 # cite  :
 # sample:python3 imgtrim_gui_ver.2.0.py  --input_dir ../assets/original_img/cbn_test_01/ --output_dir ../assets/sample_output/  --trim_width 32 --trim_height 64
 #---------------------------------------------------------------
+import keras
+from keras.utils import np_utils
+from keras.layers.convolutional import Conv2D, MaxPooling2D
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.preprocessing.image import array_to_img, img_to_array, load_img
+import keras.callbacks
+from keras.models import Sequential, model_from_json
+import numpy as np
+#import pandas as pd
+#from sklearn.model_selection import train_test_split
+#import matplotlib.pyplot as plt
+
+
+import os
 
 class myutil:
+
+    """
+    # function      : careate  directry
+    # input arg     : directry path
+    # output        : none
+    # func detail   : if not already exsits arg directry path,this function create directry.
+    """
+    def create_directory(self,directory_path):
+        if not os.path.exists(directory_path):
+            os.makedirs(directory_path)
+        else:
+            print("already exsists"+directory_path)
+
+    def create_network(self,category_num):
+        model = Sequential()
+
+        model.add(Conv2D(32, (3, 3), padding='same',input_shape=(32,32,3)))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Activation('relu'))
+        model.add(Dropout(0.5))
+
+        model.add(Conv2D(64, (3, 3), padding='same'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Activation('relu'))
+        model.add(Dropout(0.5))
+
+        #CNN_weight_2_1の時はこの部分を使わない
+        model.add(Conv2D(128, (3, 3), padding='same'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Activation('relu'))
+        model.add(Dropout(0.5))
+
+        model.add(Flatten())
+        model.add(Dense(400))
+        model.add(Activation('relu'))
+        model.add(Dropout(0.5))
+        model.add(Dense(category_num))  #分類数を決めている
+        model.add(Activation('softmax'))
+
+        #model.summary()
+
+        return model
+
+    def create_dataset(self,train_path,val_path):
+
+        TrainIMG = []
+        TrainLABEL = []
+        ValIMG = []
+        ValLABEL = []
+        TestIMG = []
+        TestLABEL = []
+        label =0
+
+        img_dirs=os.listdir(train_path)
+        for i, d in enumerate(img_dirs):
+            files0 = os.listdir(train_path+ d)
+            files1 = os.listdir(val_path+ d)
+            print(train_path+d)
+            print(val_path+d)
+            for f0 in files0:
+                img1 = img_to_array(load_img(train_path + d + '/' + f0,target_size=(32,32,3)))
+                TrainIMG.append(img1)
+                TrainLABEL.append(label)
+    
+            for f1 in files1:
+                img2 = img_to_array(load_img(val_path + d + '/' + f1,target_size=(32,32,3)))
+                ValIMG.append(img2)
+                ValLABEL.append(label)
+
+            label = label + 1
+            print("now:" + img_dirs[i])
+
+        TrainIMG = np.asarray(TrainIMG)
+        TrainLABEL = np.asarray(TrainLABEL)
+        TrainIMG = TrainIMG.astype('float32')
+        TrainIMG = TrainIMG / 255.0
+        TrainLABEL = np_utils.to_categorical(TrainLABEL, label)
+
+        ValIMG = np.asarray(ValIMG)
+        ValLABEL = np.asarray(ValLABEL)
+        ValIMG = ValIMG.astype('float32')
+        ValIMG = ValIMG / 255.0
+        ValLABEL = np_utils.to_categorical(ValLABEL, label)
+        
+        TestIMG = np.asarray(TestIMG)
+        TestLABEL = np.asarray(TestLABEL)
+        TestIMG = TestIMG.astype('float32')
+        TestIMG = TestIMG / 255.0
+        TestLABEL = np_utils.to_categorical(TestLABEL, label)
+
+        label=0
+        print("completed loading the data set")
+
+        return TrainIMG,TrainLABEL,ValIMG,ValLABEL
+
+
     def sayStr(self, str):
         print (str)
  
