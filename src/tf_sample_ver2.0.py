7249793ff337fa79f2f4817f9bc30892dbb7fab6
haxhimitsu <haxhimitsu.lab.@gmail.com>
2021-03-21 14:51:55 +0900

    add comment change test function

--- src/tf_sample_ver2.0.py
+++ src/tf_sample_ver2.0.py
@@ -8,6 +8,7 @@
 # python3 src/tf_sample_ver2.0.py  --dataset_path "{your input directory}" --log_dir "{your output directry}
 #---------------------------------------------------------------
 
+#import keras,tensorflow module
 import keras
 from keras.utils import np_utils
 from keras.layers.convolutional import Conv2D, MaxPooling2D
@@ -32,37 +33,38 @@ import argparse
 import sys
 from utils.myutils import myutil
 
+#check my module
 myutil=myutil()
 myutil.sayStr("Hello")
 
-
 #################setting GPU useage#####################
 config = tf.ConfigProto(
-    gpu_options=tf.GPUOptions(per_process_gpu_memory_fraction=0.8, # 最大値の80%まで
-        allow_growth=True # True->必要になったら確保, False->全部
+    gpu_options=tf.GPUOptions(per_process_gpu_memory_fraction=0.8, # up to 80%
+        allow_growth=True # True->gpu consumption limit enable, False->gpu consumption limit disable
       ))
 sess = sess = tf.Session(config=config)
 
 #####################################################
 
 
+#argument
 parser = argparse.ArgumentParser()
 parser.add_argument("--dataset_path",required=True,help="path to root dataset directory")
 parser.add_argument("--train_path",help="path to train_data")
 parser.add_argument("--val_path",  help="path to val_data")
+parser.add_argument("--test_path",  help="pat to test_path")
 parser.add_argument("--max_epochs", type =int ,default=100,help="set max epoch(int)")
 parser.add_argument("--batch_size", type =int ,default=32,help="set batch size 2,4,6,8,..")
 parser.add_argument("--save_weight_name", type=str,default="test",help="set_network_weight_name")
-parser.add_argument("--test_path",  help="pat to test_path")
 parser.add_argument("--log_dir", required=True, help="set_to_log_directory")
 a = parser.parse_args()
 
 log_dir=a.log_dir
-myutil.create_directory(log_dir)
-weight_filename=a.save_weight_name+".hdf5"
+myutil.create_directory(log_dir)#instance from myutil
+weight_filename=a.save_weight_name+".hdf5"#add save file name extention
 max_epochs=a.max_epochs
 
-if a.train_path is None:
+if a.train_path is None:#trainpathが引数で指定されていない場合，デフォルトでdataset_path/trains/を参照 
     train_path=a.dataset_path+"trains/"
     #print("train_path",train_path)
 else:
@@ -77,21 +79,24 @@ if a.test_path is None:
 else:
     test_path=a.test_path
 
-
+#train path 内のディレクトリ数をカウント．
+#この数が，分類数になる
 print(len(os.listdir(train_path)))
 
+#myutil.createnetworkでネットワークを作成，modelに渡す．
 model=myutil.create_network(category_num=len(os.listdir(train_path)))
 try:
     model.load_weights(os.path.join(log_dir,weight_filename))#学習結果がある場合，weightを読み込み
     print("load model")
+    #model compile
     model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
-    train_img,train_label,val_img,val_label=myutil.create_dataset(train_path,val_path)
-
-    score = model.evaluate(val_img, val_label, verbose=0)
+    train_img,train_label,val_img,val_label=myutil.create_dataset(train_path,val_path)##myutil.createdatasetでデータセットの作成
+    score = model.evaluate(val_img, val_label, verbose=0)#validation を使って評価
     print('Test loss :', score[0])
     print('Test accuracy :', score[1])
     print("pass check_acc")
-    result=myutil.acc2(model,test_path,log_dir)
+    #result=myutil.acc2(model,test_path,log_dir)#myutil.acc2でtestを使って評価
+    myutil.check_acc(model,test_path,log_dir)
     print("pass check_acc")
 
 except OSError:
